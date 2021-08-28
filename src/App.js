4c036124e09f78fc91b23abb52f9fc14dcb17c46
reza delbaz <reza.delbaz74@gmail.com>
2021-08-28 12:55:38 +0430

    first edit App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
  const App = () => {
    return ( 
-     <div>
+     <div className="test">
        <h1>todo list App - Reza</h1>
      </div>
     );
